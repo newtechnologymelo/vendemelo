c954f872ae4c393274384ae7a7d2e9d0f7feab1c
Informática Tercero <newtechnologymelo@gmail.com>
2021-08-26 21:06:06 -0300

    avance1

---
+++ login/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="algo.css">
+    <title>Inicio de sesión</title>
+    
+</head>
+<body>
+    
+<div class="fila" id="encabezado">
+
+    <div id="encabezado_izquierda" class ="encabezado_lado"></div>
+    
+    <div id="encabezado_centro"></div>
+   
+    <div id="encabezado_derecha" class ="encabezado_lado" >
+        <input id="pe" type ="button">
+    </div>
+
+</div>
+<div class="fila" id="contenido"></div>
+<div class="fila" id="pie"></div>
+    
+
+</body>
+</html>
